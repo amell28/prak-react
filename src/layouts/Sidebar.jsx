072c9feb7a9c777dedba4a3fd63c1812cef468cc
amell28 <siti24si@mahasiswa.pcr.ac.id>
2026-04-21 15:28:40 +0700

    P6 Penerapan <NavLink> untuk Active State

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -2,70 +2,99 @@ import { MdDashboard } from "react-icons/md";
 import { FaPlus } from "react-icons/fa";
 import { CiShoppingCart } from "react-icons/ci";
 import { AiOutlineCustomerService } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Sidebar() {
-    return (
-        <div id="sidebar" className="flex min-h-screen w-90 flex-col bg-white p-10 shadow-lg">
-            {/* Logo */}
-            <div id="sidebar-logo" className="flex flex-col">
-                <span id="logo-title" className="font-poppins text-[48px] text-gray-900">
-                    Sedap <b id="logo-dot" className="text-hijau">.</b>
-                </span>
-                <span id="logo-subtitle" className="font-semibold text-gray-400">Modern Admin Dashboard</span>
-            </div>
+  const menuClass = ({ isActive }) =>
+    `flex cursor-pointer items-center rounded-xl p-4  space-x-2
+        ${
+          isActive
+            ? "text-hijau bg-pink-200 font-extrabold"
+            : "text-gray-600 hover:text-hijau hover:bg-pink-200 hover:font-extrabold"
+        }`
 
-            {/* List Menu */}
-            <div id="sidebar-menu" className="mt-10">
-                <ul id="menu-list" className="space-y-3">
-                   <li>
-                        <Link
-                            id="menu-1"
-                            to="/"
-                            className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold"
-                        >
-                            <MdDashboard className="mr-4 text-xl" />
-                            Dashboard
-                        </Link>
-                        </li>
-                    <li>
-                        <Link
-                         id="menu-2" 
-                         to="/"
-                         className="hover:text-hijau flex cursor-pointer items-center rounded-xl p-4 font-medium
-                         text-gray-600 hover:bg-green-200 hover:font-extrabold">
-                        <CiShoppingCart className="mr-4 text-xl" />
-                        Orders 
-                        </Link>
-                    </li>
-                    <li> 
-                        <Link 
-                        id="menu-3" 
-                        to="/"
-                        className="hover:text-hijau flex cursor-pointer 
-                    items-center rounded-xl p-4 font-medium text-gray-600 hover:bg-green-200 hover:font-extrabold">
-                        <AiOutlineCustomerService className="mr-4 text-xl" />
-                        Customers
-                    </Link>
-                    </li>
-                </ul>
-            </div>
+  return (
+    <div
+      id="sidebar"
+      className="flex min-h-screen w-90 flex-col bg-white p-10 shadow-lg"
+    >
+      {/* Logo */}
+      <div id="sidebar-logo" className="flex flex-col">
+        <span
+          id="logo-title"
+          className="font-poppins text-[48px] text-gray-900"
+        >
+          Sedap{" "}
+          <b id="logo-dot" className="text-hijau">
+            .
+          </b>
+        </span>
+        <span id="logo-subtitle" className="font-semibold text-gray-400">
+          Modern Admin Dashboard
+        </span>
+      </div>
 
-            {/* Footer */}
-            <div id="sidebar-footer" className="mt-auto">
-                <div id="footer-card" className="bg-hijau px-4 py-2 rounded-md shadow-lg mb-10 flex items-center">
-                    <div id="footer-text" className="text-white text-sm">
-                        <span>Please organize your menus through button below!</span>
-                        <div id="add-menu-button" className="flex justify-center items-center p-2 mt-3 bg-white rounded-md space-x-2">
-                            <FaPlus className="mr-4 text-xl" />
-                            <span className="text-gray-600 flex items-center">Add Menus</span>
-                        </div>
-                    </div>
-                    <img id="footer-avatar" className="w-20 rounded-full" src="https://images.pexels.com/photos/18253842/pexels-photo-18253842.jpeg" />
-                </div>
-                <span id="footer-brand" className="font-bold text-gray-400">Sedap Restaurant Admin Dashboard</span>
-                <p id="footer-copyright" className="font-light text-gray-400">&copy; 2025 All Right Reserved</p>
+      {/* List Menu */}
+      <div id="sidebar-menu" className="mt-10">
+        <ul id="menu-list" className="space-y-3">
+          <li>
+            <NavLink id="menu-1" to="/" className={menuClass}>
+              <MdDashboard className="mr-4 text-xl" />
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              id="menu-2"
+              to="/orders"
+              className={menuClass}
+            >
+              <CiShoppingCart className="mr-4 text-xl" />
+              Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              id="menu-3"
+              to="/customers"
+             className={menuClass}
+            >
+              <AiOutlineCustomerService className="mr-4 text-xl" />
+              Customers
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {/* Footer */}
+      <div id="sidebar-footer" className="mt-auto">
+        <div
+          id="footer-card"
+          className="bg-hijau px-4 py-2 rounded-md shadow-lg mb-10 flex items-center"
+        >
+          <div id="footer-text" className="text-white text-sm">
+            <span>Please organize your menus through button below!</span>
+            <div
+              id="add-menu-button"
+              className="flex justify-center items-center p-2 mt-3 bg-white rounded-md space-x-2"
+            >
+              <FaPlus className="mr-4 text-xl" />
+              <span className="text-gray-600 flex items-center">Add Menus</span>
             </div>
+          </div>
+          <img
+            id="footer-avatar"
+            className="w-20 rounded-full"
+            src="https://images.pexels.com/photos/18253842/pexels-photo-18253842.jpeg"
+          />
         </div>
-    );
+        <span id="footer-brand" className="font-bold text-gray-400">
+          Sedap Restaurant Admin Dashboard
+        </span>
+        <p id="footer-copyright" className="font-light text-gray-400">
+          &copy; 2025 All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
 }
